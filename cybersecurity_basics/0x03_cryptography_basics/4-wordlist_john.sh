2ee6183532d650fc22db11a6ca24a3e946d6b4d8
wowkamran <KOmrOn.Ag@gmail.com>
2026-02-03 15:04:56 -0500

    12

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: '/^[a-f0-9]+:/ {print $2}' > 4-password.txt
