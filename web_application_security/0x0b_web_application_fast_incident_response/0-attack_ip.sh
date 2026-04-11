fc9601074b7a14b98b1fb7de55418bb33fe4b9dc
wowkamran <KOmrOn.Ag@gmail.com>
2026-04-11 05:09:55 -0400

    ngl

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Identify the IP address with the highest number of requests in a log file
-
-awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+cat logs.txt | awk '{print $1}' | sort | uniq -c | sort -rn | head -1 | awk '{print $2}'
